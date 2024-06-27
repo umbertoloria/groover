35dc0a1d9d92dbaa05817eec2646d029a229b4d5
Umberto Loria <umberto.loria@gmail.com>
2024-06-27 17:02:30 +0200

    Loading logics passed in TS

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,47 @@
-// import './style.css'
+import './style.css'
 
-/*document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`*/
+document.querySelector<HTMLDivElement>('#files')!.addEventListener("change", (evt) => {
+    // @ts-ignore
+    const realFiles = evt.target?.files;
+    const file = realFiles[0];
+
+    const output = [];
+    output.push("<li><strong>", escape(file.name), "</strong> </li>");
+    output.push("<div id='osmdCanvas0'/>");
+    document.querySelector<HTMLDivElement>('#list')!.innerHTML = "<ul>" + output.join("") + "</ul>";
+
+    if (!file.name.match('.*\.xml') && !file.name.match('.*\.musicxml') && false) {
+        alert('You selected a non-xml file. Please select only music xml files.');
+        return;
+    }
+
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        // @ts-ignore
+        var osmd = new opensheetmusicdisplay.OpenSheetMusicDisplay("osmdCanvas", {
+            // set options here
+            backend: "svg",
+            drawFromMeasureNumber: 1,
+            drawUpToMeasureNumber: Number.MAX_SAFE_INTEGER // draw all measures, up to the end of the sample
+        });
+        osmd
+            .load(e.target?.result)
+            .then(
+                function () {
+                    // @ts-ignore
+                    window.osmd = osmd; // give access to osmd object in Browser console, e.g. for osmd.setOptions()
+                    //console.log("e.target.result: " + e.target.result);
+                    osmd.render();
+                    // osmd.cursor.show(); // this would show the cursor on the first note
+                    // osmd.cursor.next(); // advance the cursor one note
+                }
+            );
+    };
+    if (file.name.match('.*\.mxl')) {
+        // have to read as binary, otherwise JSZip will throw ("corrupted zip: missing 37 bytes" or similar)
+        reader.readAsBinaryString(file);
+    } else {
+        reader.readAsText(file);
+    }
+}, false);
