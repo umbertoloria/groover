5cac1038b8e97f392c25243fe3319430d9eb36d7
Umberto Loria <umberto.loria@gmail.com>
2024-06-29 18:56:49 +0200

    Merging notes at max to quarters or eights depending on its start position

--- src/extract-notes-or-rests.ts
+++ src/extract-notes-or-rests.ts
@@ -2,14 +2,12 @@ import {createStack} from './stack.ts';
 
 export type NoteOrRest = {
     type: 'note' | 'rest';
+    startedOnNum16: number;
     num16: number;
     symbols: string[];
 }
 export const extractNotesOrRests = (layers: string[]) => {
     const layerLength = layers[0].length;
-
-    const max16thsToGroup = 4;
-
     const stack = createStack<NoteOrRest>();
     for (let i = 0; i < layerLength; ++i) {
         const symbols: string[] = layers.map((layer) => layer[i]);
@@ -19,24 +17,39 @@ export const extractNotesOrRests = (layers: string[]) => {
             // Note on at least one
             stack.push({
                 type: 'note',
+                startedOnNum16: i % 4,
                 num16: 1,
                 symbols,
             });
         } else {
             // Rest on all
             const lastNoteOrRest = stack.top();
-            if (lastNoteOrRest && lastNoteOrRest.num16 < max16thsToGroup) {
+            if (
+                lastNoteOrRest
+                && (
+                    (
+                        // If it starts on the first 1/16 of a 1/4, expand (potentially) to fit the whole 1/4.
+                        lastNoteOrRest.startedOnNum16 === 0 && lastNoteOrRest.num16 < 4
+                    )
+                    || (
+                        // If it starts on the third 1/16 of a 1/4, expand (potentially) to fit the whole second 1/8.
+                        lastNoteOrRest.startedOnNum16 === 2 && lastNoteOrRest.num16 < 2
+                    )
+                )
+            ) {
                 stack.pop();
                 stack.push({
                     type: lastNoteOrRest.type === 'note'
                         ? 'note'
                         : 'rest',
+                    startedOnNum16: lastNoteOrRest.startedOnNum16,
                     num16: lastNoteOrRest.num16 + 1,
                     symbols: lastNoteOrRest.symbols,
                 })
             } else {
                 stack.push({
                     type: 'rest',
+                    startedOnNum16: i % 4,
                     num16: 1,
                     symbols, // Here symbols will be all spaces.
                 });
